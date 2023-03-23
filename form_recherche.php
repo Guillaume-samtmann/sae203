ebe504a8a80faee018c7bf548b2a7d7d0f39ca2f
Guillaume-samtmann <guillaumesamtmann24@gmail.com>
2023-03-23 13:31:22 +0000

    formulaire

--- form_recherche.php
+++ form_recherche.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
-    <title>Recherchet</title>
+    <title>Recherche</title>
 </head>
 <body>
 <?php
@@ -21,16 +21,16 @@ require 'header.php';
     <input id="barre-recherche" type="text" name="texte" id="texte"><br />
     <select name="aven" id="aven-select">
     <option value="">--Aventurier disponible--</option>
-    <option value="mike">Mike Horn</option>
-    <option value="sarah">Sarah Marquis</option>
-    <option value="marco">Marco Polo</option>
-    <option value="remis">Rémis Camus</option>
-    <option value="nicolas">Nicolas Vanier</option>
-    <option value="ella">Ella Maillart</option>
-    <option value="alastair">Alastair Humphreys</option>
-    <option value="borge">Borge Ousland</option>
-    <option value="jean">Jean Troillet</option>
-    <option value="erhard">Erhard Loretan</option>
+    <option value="mike">Mike</option>
+    <option value="sarah">Sarah</option>
+    <option value="marco">Marco</option>
+    <option value="remis">Rémis</option>
+    <option value="nicolas">Nicolas</option>
+    <option value="ella">Ella</option>
+    <option value="alastair">Alastair</option>
+    <option value="borge">Borge</option>
+    <option value="jean">Jean</option>
+    <option value="erhard">Erhard</option>
     </select>
     <input id="button" type="submit" value="Afficher les expéditions réaliser par l'aventurier">
 </form>
